a5351f6ca355c02d4a6d73d6bdc8a247ea9bf173
Andre <g.andrerichards@gmail.com>
2021-07-18 21:00:19 -0400

    setup query micro

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,8 +1,4 @@
 const express = require('express')
-// const cors = require('cors')
-const {
-    randomBytes
-} = require('crypto')
 const axios = require('axios')
 
 const app = express()
@@ -11,7 +7,6 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.json())
-// app.use(cors())
 
 app.post('/events', (req, res) => {
     const event = req.body

f8f7cdef550b9a763e56b52326a9a84db5dd67e6
Andre <g.andrerichards@gmail.com>
2021-07-19 00:28:21 -0400

    update client UI for test comment moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,21 @@ import React from "react"
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li style={{fontSize: '.85rem'}} key={comment.id}>{comment.content}</li>
+    let content
+
+    if (comment.status === 'approved') {
+      content = comment.content
+    }
+
+    if (comment.status === 'pending') {
+      content = <em>Comment awaiting approval</em>
+    }
+
+    if (comment.status === 'rejected') {
+      content = <em>Comment not approved</em>
+    }
+
+    return <li style={{fontSize: '.85rem'}} key={comment.id}>{content}</li>
   })
 
   return <ul>{renderedComments}</ul>
